e07ea300cf23b75e535932d36269a56515a923c6
Andrew González <andrewalangm@gmail.com>
2021-01-08 11:19:54 -0600

    🧪 Modificando la prueba unitaria y removiendo los mocks

--- src/test/java/codelapps/transition_fa/AuditManagerTest.java
+++ src/test/java/codelapps/transition_fa/AuditManagerTest.java
@@ -1,11 +1,8 @@
 package codelapps.transition_fa;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
-import java.nio.file.Path;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -16,32 +13,25 @@ import org.junit.jupiter.api.Test;
 
 public class AuditManagerTest {
 
-    public List<String> sampleLines() {
-        List<String> lines = new ArrayList<String>();
-        lines.add("Andrew;2020-04-04T16:30:00");
-        lines.add("Alan;2020-04-09T16:30:00");
-        lines.add("Dany;2020-04-07T16:30:00");
-        return lines;
-    }
+  public List<String> sampleLines() {
+    List<String> lines = new ArrayList<String>();
+    lines.add("Andrew;2020-04-04T16:30:00");
+    lines.add("Alan;2020-04-09T16:30:00");
+    lines.add("Dany;2020-04-07T16:30:00");
+    return lines;
+  }
 
-    @Test
-    public void a_new_file_is_created_when_current_file_is_overflows() throws IOException, ParseException {
-        /*// Arrange
-        String[] filesList = { "audit_1.txt", "audit_2.txt" };
-
-        FileSystem fileSystemMock = mock(FileSystem.class);
-        when(fileSystemMock.getFilesList("assets")).thenReturn(filesList);
-        when(fileSystemMock.readAllLines(Path.of("assets", "audit_2.txt"))).thenReturn(sampleLines());
-        
-        AuditManager sut = new AuditManager(3, "assets", fileSystemMock);
-        Date date = new SimpleDateFormat("MM/dd/yyyy").parse("6/27/2020");
-
-        // Act
-        sut.addRecord("Codelapps", date);
-
-        // Assert
-        verify(fileSystemMock).writeNewFile(Path.of("assets", "audit_3.txt"),
-                new String("Codelapps;Sat Jun 27 00:00:00 CDT 2020").getBytes());*/
-
-    }
+  @Test
+  public void a_new_file_is_created_when_current_file_is_overflows() throws IOException, ParseException {
+    // Arrange
+    FileContent[] files = new FileContent[1];
+    files[0] = new FileContent("audit_1.txt", sampleLines());
+    Date date = new SimpleDateFormat("MM/dd/yyyy").parse("6/27/2020");
+    AuditManager sut = new AuditManager(3);
+    // Act
+    FileUpdate update = sut.addRecord(files, "Codelapps", date);
+    // Assert
+    assertEquals(update.filePath, "audit_2.txt");
+    assertEquals(update.newLine, "Codelapps;Sat Jun 27 00:00:00 CDT 2020");
+  }
 }
